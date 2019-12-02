19741e5c1de0389a37bc7813a566a6445b461376
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-12-02 00:02:15 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+console.log(
+  `
+╭───────────────────────────────────────────╮
+│                                           │
+│             \u001b[32m\u001b[1mJacob Gillespie\u001b[0m               │
+│                                           │
+│      \u001b[38;5;102mWork:\u001b[0m  Software Engineer at Thorn    │
+│       \u001b[38;5;102mWeb:\u001b[0m  \u001b[34mjacobwgillespie.com\u001b[0m           │
+│    \u001b[38;5;102mGitHub:\u001b[0m  \u001b[34mgithub.com/jacobwgillespie\u001b[0m    │
+│   \u001b[38;5;102mTwitter:\u001b[0m  \u001b[34mtwitter.com/jacobwgillespie\u001b[0m   │
+│                                           │
+╰───────────────────────────────────────────╯
+`,
+)
