dab5b7ee7fcd116efd31e54af687b6d1641d51bf
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-04-14 10:17:34 +0100

    Update employment

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(
 │                                           │
 │             \u001b[32m\u001b[1mJacob Gillespie\u001b[0m               │
 │                                           │
-│      \u001b[38;5;102mWork:\u001b[0m  Software Engineer at Thorn    │
+│      \u001b[38;5;102mWork:\u001b[0m  Software Engineer at EraDB    │
 │       \u001b[38;5;102mWeb:\u001b[0m  \u001b[34mjacobwgillespie.com\u001b[0m           │
 │    \u001b[38;5;102mGitHub:\u001b[0m  \u001b[34mgithub.com/jacobwgillespie\u001b[0m    │
 │   \u001b[38;5;102mTwitter:\u001b[0m  \u001b[34mtwitter.com/jacobwgillespie\u001b[0m   │
